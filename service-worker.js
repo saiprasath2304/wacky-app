cb425d4132399922912a119fa3b089e4368ba53f
Sprocker23 <71890332+saiprasath2304@users.noreply.github.com>
2025-04-27 22:58:16 +0530

    name change

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
       caches.open('wacky-cache').then((cache) => 
         cache.addAll([
-          './wacky.html',
+          './index.html',
           './manifest.json',
           './icon-192.png',
           './icon-512.png',

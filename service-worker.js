eba6604a72ec31ef4f1a667faada8ea3c7a3cbac
Sprocker23 <71890332+saiprasath2304@users.noreply.github.com>
2025-04-27 23:08:36 +0530

    app patch update

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,43 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('wacky-cache').then((cache) => 
-        cache.addAll([
-          './index.html',
-          './manifest.json',
-          './icon-192.png',
-          './icon-512.png',
-          'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js',
-          'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'
-        ])
-      )
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((response) => response || fetch(e.request))
-    );
-  });
-  
+const CACHE_NAME = 'wacky-cache-v2'; // <-- Change the version when you update
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
